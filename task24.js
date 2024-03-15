b367cd4cf53b1f5cacdc2195c2f1013b9609c6ae
Serega <61983829+Sergey-Sverdlov@users.noreply.github.com>
2024-03-16 01:43:57 +0300

    add linked_list

--- task24.js
+++ task24.js
@@ -1,19 +1,53 @@
-// Замыкание
-// Карреляция
-const Counter = () => {
-    let value = 0
-    return function () {
-        value += 1
-        return value
+// Двусвязный список
+
+class LinkedList {
+    constructor() {
+        this.size = 0
+        this.root = null
+    }
+
+    add(value) {
+        if (this.size === 0) {
+            this.root = new Node(value)
+            this.size += 1
+            return true
+        }
+        let node = this.root
+        while (node.next) {
+            node = node.next
+        }
+        let newNode = new Node(value)
+        node.next = newNode
+        this.size += 1
+    }
+
+    getSize() {
+        return this.size
+    }
+
+    print() {
+        let result = []
+        let node = this.root
+        while (node) {
+            result.push(node.value)
+            node = node.next
+        }
+        console.log(result)
     }
 }
 
-const Closure = (a) => {
-    return function (b) {
-        return a + b
+class Node {
+    constructor(val) {
+        this.value = val
+        this.next = null
     }
 }
 
-let c = Counter()
+const list = new LinkedList()
 
-console.log(Closure(5)(3))
+list.add(2)
+list.add(5)
+list.add(7)
+list.add(1)
+
+list.print()
